2fbfa8047c16ab06046b379df6151f0af0e9efa2
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-03-24 14:26:18 +0700

    practices

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,4 +9,10 @@ export class UserController {
   test(): string[] {
     return this.userService.test();
   }
+
+  @Get('users')
+  findAll() {
+    return this.userService.findAll();
+  }
+  
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from './user.interface'
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class UserService {
+
+  private readonly dataPath = path.join(process.cwd(), 'data', 'users.json');
+
   test(): string[] {
     return [];
+  }
+
+  findAll(): IUser[] {
+    const rawData = fs.readFileSync(this.dataPath, 'utf-8');
+    const users = JSON.parse(rawData) as IUser[];
+    return users;
   }
 }
